432d9455e7d5874c2c0c191ad10345ce12420bed
ThienDaoXuan <51303840@hcmut.edu.vn>
2016-11-15 16:25:47 +0700

    dfjhsdj

---
+++ Test10k/generateFile.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <system.io>
+
+int main(int argc, char *argv[]){
+	int len = 0;
+	len = atoi(argv[1]);
+
+	int i;
+	FILE* file = fopen("myMatrixA","wb");
+	FILE* file2 = fopen("myMatrixB","wb");
+	for ( i = 0 ; i < len; i++ ){
+		float *buff = (float*)malloc(len);
+		float *buff2 = (float*)malloc(len);
+		for(j = 0; j < len; ++j){
+			buff[j] = i + j;
+			buff2[j] = j * i;
+			fwrite(buff, sizeof(float), len, file);
+			fwrite(buff, sizeof(float), len, file2);
+		}
+		free(buff); free(buff2);
+	}
+
+}
